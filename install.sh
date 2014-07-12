f570f21db4d1fb9b9092eed0fe2b076bbc8a0279
lizhizhou <lzz@meteroi.com>
2014-07-11 21:30:00 -0700

    install script update
    Signed-off-by: lizhizhou <lzz@meteroi.com>

--- install.sh
+++ install.sh
@@ -5,27 +5,11 @@ set lophilo_user     lophilo
 #the ip or hostname of lophilo board    
 set lophilo_password lab123     
 #the user name in lophilo board
-set lophilo_address  192.168.1.4   
+set lophilo_address  192.168.1.5   
 #the directory to store the program on lophilo board
 set lophilo_path     ~/ide/          
 #download program
-spawn scp -r index.html $lophilo_user@$lophilo_address:$lophilo_path
-set timeout 50
-expect "password:"
-set timeout 50
-send "$lophilo_password\r"
-set timeout 50
-send "exit\r"
-expect eof
-spawn scp -r server.js $lophilo_user@$lophilo_address:$lophilo_path
-set timeout 50
-expect "password:"
-set timeout 50
-send "$lophilo_password\r"
-set timeout 50
-send "exit\r"
-expect eof
-spawn scp -r src $lophilo_user@$lophilo_address:$lophilo_path
+spawn scp -r ./ $lophilo_user@$lophilo_address:$lophilo_path
 set timeout 50
 expect "password:"
 set timeout 50
